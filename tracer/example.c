276dd1f9674a381cf7f8bfbaa2f8ccbaec3b6a76
Jamie Fox <jf549@cam.ac.uk>
2017-03-08 22:06:05 +0000

    Add nested loop test case

--- tracer/example.c
+++ tracer/example.c
@@ -4,6 +4,14 @@ int main(void) {
   x = x + 1;
   y = y + 4;
   x = x + 2;
-  for (int i = 0; i < 8; ++i) x += 1;
+  for (int i = 0; i < 5; ++i) {
+    y += 1;
+    for (int j = 0; j < 2; ++j) {
+      x += 1;
+      for (int k = 0; k < 2; ++k) {
+        x += 2;
+      }
+    }
+  }
   return 0;
 }
