8cb34ed806b848247e74168f223c6617fbb6ccb4
Jamie Fox <jf549@cam.ac.uk>
2017-03-09 21:27:43 +0000

    Add debug prints to test code for easier correctness checking

--- tracer/example.c
+++ tracer/example.c
@@ -1,17 +1,21 @@
+#include <stdio.h>
+
 int main(void) {
+  printf("FUNCTION START\n");
   int x = 3;
   char y = 2;
   x = x + 1;
   y = y + 4;
-  x = x + 2;
-  for (int i = 0; i < 5; ++i) {
+  printf("LOOP ENTRY 1\n");
+  for (int i = 0; i < 3; ++i) {
+    printf("LOOP BODY 1\n");
     y += 1;
+    printf("LOOP ENTRY 2\n");
     for (int j = 0; j < 2; ++j) {
+      printf("LOOP BODY 2\n");
       x += 1;
-      for (int k = 0; k < 2; ++k) {
-        x += 2;
-      }
     }
   }
+  printf("FUNCTION END\n");
   return 0;
 }
