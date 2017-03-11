ad955fc87191dd9cefa37425ebf15298b6025a9c
Jamie Fox <jf549@cam.ac.uk>
2017-03-11 16:44:05 +0000

    Abstract inserting call into function to save repeated code

--- tracer/instrument/InstrumentPass.cpp
+++ tracer/instrument/InstrumentPass.cpp
@@ -87,36 +87,31 @@ namespace {
       }
     }
 
+    template<typename InstT>
+    void insertCall(Function& fn, InstT* inst) const {
+      auto& ctx = fn.getContext();
+      // Runtime library function to call on load/store
+      auto memoryEventFn = fn.getParent()->getOrInsertFunction("memoryEvent", Type::getVoidTy(ctx),
+        Type::getInt32Ty(ctx), Type::getInt8PtrTy(ctx), Type::getInt64Ty(ctx), nullptr);
+      IRBuilder<> builder(inst); // Insert function call before instruction
+      Value* args[3]; // TODO use SmallVector?
+      args[0] = ConstantInt::get(Type::getInt32Ty(ctx), std::is_same<InstT, LoadInst>::value ? LOAD : STORE);
+      args[1] = builder.CreateBitCast(inst->getPointerOperand(), Type::getInt8PtrTy(ctx));
+      args[2] = ConstantInt::get(Type::getInt64Ty(ctx), reinterpret_cast<uintptr_t>(inst));
+      builder.CreateCall(memoryEventFn, args);
+    }
+
     // Next we perform a peephole analysis over each instruction in the function. For every load
     // or store instruction, we insert a function call to our dynamic library. The function is
     // provided with (1) whether the memory instruction is a store or load, (2) the address
     // accessed and (3) the line number of the instruction in the IR.
     void instrumentMemoryEvents(Function& fn) const {
-      auto& ctx = fn.getContext();
-
       for (auto& block : fn) {
         for (auto& inst : block) {
-          if (isa<LoadInst>(inst) || isa<StoreInst>(inst)) {
-            // Runtime library function to call on load/store
-            auto memoryEventFn = fn.getParent()->getOrInsertFunction("memoryEvent", Type::getVoidTy(ctx),
-              Type::getInt32Ty(ctx), Type::getInt8PtrTy(ctx), Type::getInt64Ty(ctx), nullptr);
-            Value* args[3]; // TODO use SmallVector?
-            IRBuilder<> builder(&inst);
-            builder.SetInsertPoint(&block, ++builder.GetInsertPoint()); // Insert call after op
-
-            if (auto loadInst = dyn_cast<LoadInst>(&inst)) {
-              args[0] = ConstantInt::get(Type::getInt32Ty(ctx), LOAD);
-              args[1] = builder.CreateBitCast(loadInst->getPointerOperand(), Type::getInt8PtrTy(ctx));
-              args[2] = ConstantInt::get(Type::getInt64Ty(ctx), reinterpret_cast<uintptr_t>(loadInst));
-
-            } else {
-              auto storeInst = cast<StoreInst>(&inst);
-              args[0] = ConstantInt::get(Type::getInt32Ty(ctx), STORE);
-              args[1] = builder.CreateBitCast(storeInst->getPointerOperand(), Type::getInt8PtrTy(ctx));
-              args[2] = ConstantInt::get(Type::getInt64Ty(ctx), reinterpret_cast<uintptr_t>(storeInst));
-            }
-
-            builder.CreateCall(memoryEventFn, args);
+          if (auto loadInst = dyn_cast<LoadInst>(&inst)) {
+            insertCall(fn, loadInst);
+          } else if (auto storeInst = dyn_cast<StoreInst>(&inst)) {
+            insertCall(fn, storeInst);
           }
         }
       }
