040b9565e756920e719f59da630d6ac579eb9869
Jamie Fox <jf549@cam.ac.uk>
2017-03-19 18:56:11 +0000

    Use add_llvm_loadable_module to build LLVM pass

--- tracer/instrument.sh
+++ tracer/instrument.sh
@@ -1,6 +1,6 @@
 clang -c -emit-llvm example.c
 opt -loop-simplify example.bc -o example_simplify.bc
-opt -load build/instrument/libInstrumentPass.so -instrument example_simplify.bc -o example_inst.bc
+opt -load build/instrument/InstrumentPass.dylib -instrument example_simplify.bc -o example_inst.bc
 llc example_inst.bc
 clang -S tracerlib.c
 clang tracerlib.s example_inst.s
