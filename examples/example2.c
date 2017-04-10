36defee80633f403d9d6b9531ca3a815dd739f0c
Jamie Fox <jf549@cam.ac.uk>
2017-04-10 20:21:22 +0100

    Fix example

--- examples/example2.c
+++ examples/example2.c
@@ -1,21 +1,15 @@
-#include <stdio.h>
-
 int main(void) {
-  printf("FUNCTION START\n");
   int x = 3;
   char y = 2;
   x = x + 1;
   y = y + 4;
-  printf("LOOP ENTRY 1\n");
-  for (int i = 0; i < 3; ++i) {
-    printf("LOOP BODY 1\n");
+
+  for (int i = 0; i < 6; ++i) {
     y += 1;
-    printf("LOOP ENTRY 2\n");
-    for (int j = 0; j < 2; ++j) {
-      printf("LOOP BODY 2\n");
+    for (int j = 0; j < 6; ++j) {
       x += 1;
     }
   }
-  printf("FUNCTION END\n");
+
   return 0;
 }
