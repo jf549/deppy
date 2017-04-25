d9771f6f95bcb56c9436d47823c37ee0d4abfb9c
Jamie Fox <jf549@cam.ac.uk>
2017-04-25 21:49:35 +0100

    Reimplement dynamic gcd with arbitrary precision

--- analyser/Stride.cpp
+++ analyser/Stride.cpp
@@ -1,7 +1,6 @@
 #include "Stride.h"
-#include <lib/gcd.h>
 
-#include <boost/math/common_factor_rt.hpp>
+#include <gmpxx.h>
 
 #include <algorithm>
 
@@ -11,28 +10,35 @@ namespace analyser {
     if (other.base < base) {
       return other.numDependences(*this);
     }
+    mpz_class low0 = static_cast<unsigned long>(base);
+    mpz_class dist0 = static_cast<unsigned long>(stride);
+    mpz_class dist1 = static_cast<unsigned long>(other.stride);
 
-    auto low = other.base;
-    auto high = std::min(limit, other.limit);
+    mpz_class low = static_cast<unsigned long>(other.base);
+    mpz_class high = static_cast<unsigned long>(std::min(limit, other.limit));
 
     if (high < low) {
       return 0;
     }
 
-    auto delta = (stride - ((low - base) % stride)) % stride;
+    mpz_class delta = (dist0 - ((low - low0) % dist0)) % dist0;
 
-    if (delta % boost::math::gcd(stride, other.stride) != 0) {
+    if (delta % gcd(dist0, dist1) != 0) {
       return 0;
     }
 
-    auto length = high - low + 1;
+    mpz_class gcd, x, y;
+    mpz_gcdext(gcd.get_mpz_t(), x.get_mpz_t(), y.get_mpz_t(), dist0.get_mpz_t(), dist1.get_mpz_t());
 
-    auto res = lib::egcd(stride, other.stride);
-    auto lcm = (stride / res.gcd) * other.stride; // gcd divides both; more efficient to divide only one
-    auto offset = (other.stride * res.y * delta / res.gcd + lcm) % lcm;
-    auto result = (length - (offset + 1) + lcm) / lcm;
+    mpz_class lcm = (dist0 / gcd) * dist1; // gcd divides both; more efficient to divide only one
+    mpz_class offset = (dist1 * y * delta / gcd + lcm) % lcm;
+    mpz_class result = (high - low + 1 - (offset + 1) + lcm) / lcm;
 
-    return result;
+    if (result < 0) {
+      return 0;
+    } else {
+      return result.get_ui();
+    }
   }
 
   bool Stride::isDependent(uint64_t addr) const {
