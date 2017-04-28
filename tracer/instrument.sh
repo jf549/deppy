0867744966d019366b053574622cefad0fe85d62
Jamie Fox <jf549@cam.ac.uk>
2017-04-28 20:35:32 +0100

    Improve instrument script

--- tracer/instrument.sh
+++ tracer/instrument.sh
@@ -2,12 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-clang -g -emit-llvm $1 -c -o $DIR/tmp0.bc
-
-opt -loop-simplify $DIR/tmp0.bc -o $DIR/tmp1.bc
-opt -load $DIR/loopinstrument/LoopInstrumentPass.dylib -instrument-loops $DIR/tmp1.bc -o $DIR/tmp2.bc
-opt -load $DIR/meminstrument/MemInstrumentPass.dylib -instrument-mem $DIR/tmp2.bc -o $DIR/tmp3.bc
-
-llc $DIR/tmp3.bc
-
-clang -L$DIR $DIR/tmp3.s -ltracer -o $DIR/tracer
+clang -g -c -emit-llvm -o - "$@" |
+opt -loop-simplify |
+opt -load $DIR/loopinstrument/LoopInstrumentPass.dylib -instrument-loops |
+opt -load $DIR/meminstrument/MemInstrumentPass.dylib -instrument-mem |
+clang -x ir -L$DIR -ltracer -O2 -o $DIR/tracer -
