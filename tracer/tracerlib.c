22a09c16b46f81bfc0549f22a7efd50229a3901d
Jamie Fox <jf549@cam.ac.uk>
2017-04-29 21:45:03 +0100

    Massively improved IO

--- tracer/tracerlib.c
+++ tracer/tracerlib.c
@@ -1,36 +1,59 @@
 #include "tracerlib.h"
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
-void loopEvent(event_t type) {
-  switch (type) {
-    case LOOP_ENTRY:
-    case LOOP_ITER:
-    case LOOP_EXIT:
-      fwrite(&type, sizeof(type), 1, stdout);
-      break;
+#define BUFLEN 4096
+static char *buf;
+static char *ptr;
 
-    case LOAD:
-    case STORE:
-      perror("Invalid LoopEvent\n");
-      break;
+void serialise_uint64(uint64_t val);
+void serialise_event(event_t val);
+void init_buf(void);
+void flush_buf(void);
+
+void serialise_uint64(uint64_t val) {
+  ptr[0] = (char) (val >> 56);
+  ptr[1] = (char) (val >> 48);
+  ptr[2] = (char) (val >> 40);
+  ptr[3] = (char) (val >> 32);
+  ptr[4] = (char) (val >> 24);
+  ptr[5] = (char) (val >> 16);
+  ptr[6] = (char) (val >> 8);
+  ptr[7] = (char) val;
+  ptr += 8;
+}
+
+void serialise_event(event_t val) {
+  ptr[0] = (char) val;
+  ++ptr;
+}
+
+void init_buf(void) {
+  buf = (char *) malloc(BUFLEN);
+  ptr = buf;
+  if (!buf) {
+    printf("Failed to allocate buffer!");
+    exit(1);
   }
 }
 
-void memoryEvent(event_t type, void *addr, uint64_t pc) {
-  mem_event_t me = {pc, (uint64_t) addr};
+void flush_buf(void) {
+  write(2, buf, ptr - buf);
+  ptr = buf;
+}
 
-  switch (type) {
-    case LOAD:
-    case STORE:
-      fwrite(&type, sizeof(type), 1, stdout);
-      fwrite(&me, sizeof(me), 1, stdout);
-      break;
+void loopEvent(event_t event) {
+  if (!buf) init_buf();
+  serialise_event(event);
+  if (ptr - buf > BUFLEN - 17) flush_buf();
+}
 
-    case LOOP_ENTRY:
-    case LOOP_ITER:
-    case LOOP_EXIT:
-      perror("Invalid MemoryEvent\n");
-      break;
-  }
+void memoryEvent(event_t event, void *addr, uint64_t pc) {
+  if (!buf) init_buf();
+  serialise_event(event);
+  serialise_uint64(pc);
+  serialise_uint64((uint64_t)addr);
+  if (ptr - buf > BUFLEN - 17) flush_buf();
 }
