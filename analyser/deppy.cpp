ebc18ef2461b815d33584a2df4e43437ac7c48f6
Jamie Fox <jf549@cam.ac.uk>
2017-04-30 17:32:06 +0100

    Rewrite analyser to use good IO

--- analyser/deppy.cpp
+++ analyser/deppy.cpp
@@ -1,11 +1,14 @@
 #include "StrideLoop.h"
 #include <lib/event.h>
 
+#include <array>
 #include <chrono>
 #include <ctime>
 #include <iostream>
 #include <iomanip>
 #include <stack>
+
+#define BUFLEN 4096
 
 using namespace analyser;
 
@@ -15,44 +18,68 @@ int main() {
   auto tStart = std::chrono::steady_clock::now();
 #endif
 
-  mem_event_t memEvent;
-  event_t event;
   std::stack<StrideLoop> loopStack;
+  std::array<char, BUFLEN> buf;
+  auto front = begin(buf);
+  auto back = cend(buf);
 
-  while (std::cin.read(reinterpret_cast<char*>(&event), sizeof(event))) {
-    switch (event) {
-      case LOOP_ENTRY:
-        if (loopStack.empty()) {
-          loopStack.emplace(); // Top level loop
-        } else {
-          loopStack.emplace(&loopStack.top()); // Nested loop
-        }
-        break;
+  // Increase IO performance
+  std::ios_base::sync_with_stdio(false); // Prevents use of C IO functions
+  std::cin.tie(nullptr); // Prevents in/out interaction with console
 
-      case LOOP_ITER:
-        if (!loopStack.empty()) {
-          loopStack.top().iterate();
-        }
-        break;
+  while (std::cin.read(front, buf.size()) || std::cin.gcount()) {
+    auto it = front;
+    auto end = front + std::cin.gcount();
 
-      case LOOP_EXIT:
-        if (!loopStack.empty()) {
-          loopStack.top().terminate();
-          loopStack.pop();
-        }
-        break;
+    while (it != end) {
+      event_t event = static_cast<event_t>(*it++);
 
-      case LOAD:
-      case STORE:
-        if (std::cin.read(reinterpret_cast<char*>(&memEvent), sizeof(memEvent))) {
-          if (!loopStack.empty()) {
-            loopStack.top().memoryRef(memEvent.pc, memEvent.addr, event == STORE);
+      switch (event) {
+        case LOOP_ENTRY:
+          if (loopStack.empty()) {
+            loopStack.emplace(); // Top level loop
+          } else {
+            loopStack.emplace(&loopStack.top()); // Nested loop
           }
-        } else {
-          std::cerr << "Did not receive memory event\n";
-          return 1;
-        }
-        break;
+          break;
+
+        case LOOP_ITER:
+          if (!loopStack.empty()) {
+            loopStack.top().iterate();
+          }
+          break;
+
+        case LOOP_EXIT:
+          if (!loopStack.empty()) {
+            loopStack.top().terminate();
+            loopStack.pop();
+          }
+          break;
+
+        case LOAD:
+        case STORE:
+          if (end - it < 16) {
+            end = std::copy(it, end, front);
+            it = front;
+            std::cin.read(end, back - end);
+            end += std::cin.gcount();
+            if (end - it < 16) {
+              std::cerr << "Did not receive memory event\n";
+              return 1;
+            }
+          }
+          {
+            uint64_t pc, addr;
+            memcpy(&pc, it, sizeof(pc));
+            it += sizeof(pc);
+            memcpy(&addr, it, sizeof(addr));
+            it += sizeof(addr);
+            if (!loopStack.empty()) {
+              loopStack.top().memoryRef(pc, addr, event == STORE);
+            }
+          }
+          break;
+      }
     }
   }
 
