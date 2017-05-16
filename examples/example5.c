ef370bebe22bf9341923a3935848aab05b83a262
Jamie Fox <jf549@cam.ac.uk>
2017-05-16 16:46:53 +0100

    Add example 5

---
+++ examples/example5.c
@@ -0,0 +1,16 @@
+// An example with heap-allocated memory.
+#include <stdlib.h>
+
+int main(void) {
+  int *a = malloc(10 * sizeof(a));
+  int *ptr = a, *end = a + 10;
+
+  *ptr++ = 0;
+
+  while (ptr != end) {
+    *ptr = *(ptr - 1) + 1;
+    ++ptr;
+  }
+
+  free(a);
+}
