94c3daf7324dd0c84bb85b40402e7cf6889896a1
Jamie Fox <jf549@cam.ac.uk>
2017-05-16 15:59:35 +0100

    Update example2

--- examples/example2.c
+++ examples/example2.c
@@ -1,15 +1,10 @@
-/// An example of a loop with two strides and loop-carried dependences between them.
+// An example of a loop with two strides and loop-carried dependences between them.
 int main(void) {
-  char a[30];
-  char x = 0;
+  int x, i, a[30];
 
-  for (int i = 0; i < 30; ++i) {
-    a[i] = i;
-  }
-
-  for (int n = 0; n <= 6; ++n) {
-    a[2*n + 10] = 1; // Stride 1 (Write)
-    x = a[3*n + 11]; // Stride 2 (Read)
+  for (i = 0; i <= 6; ++i) {
+    a[2*i + 10] = 1; // Stride 1 (Write)
+    x = a[3*i + 11]; // Stride 2 (Read)
   }
 
   return 0;
