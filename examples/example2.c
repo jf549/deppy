5b3823dca78b531e14851bfea63ad7e14d135df3
Jamie Fox <jf549@cam.ac.uk>
2017-05-16 16:29:35 +0100

    Fix example 2

--- examples/example2.c
+++ examples/example2.c
@@ -1,6 +1,6 @@
 // An example with two strides and loop-carried dependences between them.
 int main(void) {
-  int x, i, a[30];
+  int x, i, a[30] = { 0 };
 
   for (i = 0; i < 7; ++i) {
     a[2*i + 10] = 1; // Stride 1 (Write)
