52e639e2d47ed5c7ae388e074d9cfb1569803507
Jamie Fox <jf549@cam.ac.uk>
2017-05-19 15:17:38 +0100

    Added bounded buffer tests

--- test/test_lib.cpp
+++ test/test_lib.cpp
@@ -1,10 +1,50 @@
-#include <lib/gcd.h>
+#include <lib/BoundedBuffer.hpp>
+#include <lib/event.h>
 
 #include <catch.hpp>
 
-TEST_CASE("EGCD is computed", "[egcd]") {
-  auto res = lib::egcd(240, 46);
-  REQUIRE(res.gcd == 2);
-  REQUIRE(res.x == -9);
-  REQUIRE(res.y == 47);
+#include <array>
+#include <future>
+
+TEST_CASE("Bounded buffer can be accessed sequentially", "[boundedbuf]") {
+  lib::BoundedBuffer<event_t> buf;
+  std::vector<event_t> a{ { LOAD, LOAD, STORE, LOOP_ENTRY, LOOP_ITER, LOOP_EXIT } };
+  std::vector<event_t> res;
+
+  for (auto item : a) {
+    buf.produce(item);
+  }
+
+  buf.produce(SENTINAL);
+
+  event_t item;
+
+  while ((item = buf.consume()) != SENTINAL) {
+    res.push_back(item);
+  }
+
+  REQUIRE(a == res);
+}
+
+TEST_CASE("Bounded buffer can be accessed concurrently", "[boundedbuf]") {
+  lib::BoundedBuffer<int> buf(3);
+  std::array<int, 10> a{ { -1, -2, -3, 38, 21, 0, 0, 0, INT_MAX, INT_MIN } };
+
+  auto futProd = std::async([&buf, &a]{
+    for (auto item : a) {
+      buf.produce(item);
+    }
+  });
+
+  auto futCon = std::async([&buf]{
+    std::array<int, 10> res;
+    for (unsigned i = 0; i < 10; ++i) {
+      res[i] = buf.consume();
+    }
+    return res;
+  });
+
+  futProd.get();
+
+  REQUIRE(futCon.get() == a);
 }
